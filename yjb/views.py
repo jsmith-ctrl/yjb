8f648c9ccd5753042a8148e5d32f8a207efccfa7
W.O. <oluwanifemi2710@gmail.com>
2026-08-20 12:58:12 -0600

    added navbar links

--- yjb/views.py
+++ yjb/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -12,24 +13,6 @@ def default(request):
     # Renders 'myapp/index.html' with the context data
     return render(request, "yjb/default.html", context)
 
-def home(request):
-    return render(request, "yjb/home.html", {})
-
-def job_listings(request):
-    return render(request, "yjb/job-listings.html")
-
-def about(request):
-    return render(request, "yjb/about.html")
-
-def contact(request):
-    return render(request, "yjb/contact.html")
-
-def applicants(request):
-    return render(request, "yjb/employer/applicants.html")
-
-def employee_signup(request):
-    return render(request, "yjb/employee/sign-up-employee.html")
-
-def employer_signup(request):
-    return render(request, "yjb/employer/sign-up-employer.html")
-
+def helloworld(request):
+     debug_string = "<br>".join(f"<b>{k}</b>: {v}" for k, v in request.META.items())
+    return HttpResponse(debug_string, content_type="text/html")
