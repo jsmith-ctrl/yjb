6092b81c79b855e037db9fdd48a4aedfaba031e3
Jeremy-Linux <smitty_1616@hotmail.com>
2026-08-27 10:41:30 -0600

    add admin for jobs

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Job
 
-# Register your models here.
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "company",
+        "location",
+        "salary",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "company",
+        "location",
+        "description",
+    )
+
+    list_filter = (
+        "company",
+        "location",
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
+
+    readonly_fields = ("created_at",)
+
+    fieldsets = (
+        ("Job Information", {
+            "fields": (
+                "title",
+                "company",
+                "description",
+            )
+        }),
+        ("Location & Compensation", {
+            "fields": (
+                "location",
+                "salary",
+            )
+        }),
+        ("System Information", {
+            "fields": (
+                "created_at",
+            )
+        }),
+    )
